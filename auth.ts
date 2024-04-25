6823422b34bde3b5e8ba9584977408eaeedb78ef
unknown <jghuynh3@gmail.com>
2024-04-24 17:02:11 -0700

    Edited getUser() to check if hashed password and original password match

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'; // hashes password
 
 /**
  * Queries the user from the database
@@ -29,6 +29,19 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
+
+          if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+
+          // see if passwords match between hasehd password and original
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+        console.log('Invalid credentials');
+        return null;
       },
     }),
   ], // different login options. Authorize helps us
